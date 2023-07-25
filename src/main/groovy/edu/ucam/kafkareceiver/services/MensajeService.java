36fcabcf65dd6be6335d936e7afaa9bf4268ebfb
JaArques <jaalcolea@ucam.edu>
2023-07-25 14:09:44 +0200

    Patron factory completo

--- src/main/groovy/edu/ucam/kafkareceiver/services/MensajeService.java
+++ src/main/groovy/edu/ucam/kafkareceiver/services/MensajeService.java
@@ -2,7 +2,9 @@ package edu.ucam.kafkareceiver.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.ucam.kafkareceiver.entity.Mensaje;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import edu.ucam.kafkareceiver.entity.EventoDTO;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,13 +16,17 @@ public class MensajeService {
     ObjectMapper objectMapper = new ObjectMapper();
 
     @KafkaListener(topics = "test_topic")
-    public void receiveMessage(Mensaje mensaje) {
-        System.out.println("Mensaje recibido: " + mensaje.getId() + " fecha: " + mensaje.getDate() + " mensaje: " + mensaje.getMensaje());
+    public void receiveMessage(ConsumerRecord<String, String> kafkaMessage) throws JsonProcessingException {
+        objectMapper.registerModule(new JavaTimeModule());
+        String jsonString = kafkaMessage.value();
+        EventoDTO jsonObject = objectMapper.readValue(jsonString, EventoDTO.class);
+        System.out.println("Received message: " + jsonObject);
     }
 
     @RabbitListener(queues = {"MQ_DEMO"})
     public void receiveManage(@Payload String message) throws JsonProcessingException {
-        Object jsonObject = objectMapper.readValue(message, Object.class);
+        objectMapper.registerModule(new JavaTimeModule());
+        EventoDTO jsonObject = objectMapper.readValue(message, EventoDTO.class);
         System.out.println("Received message: " + jsonObject);
     }
 }
